7b532a3a75259766c8117bff977ad5759c21f3c7
Emil Winterhalder <emil.winterhalder@tum.de>
2025-12-14 20:24:58 +0100

    tweaking transfer funcs

--- src/analyses/wp_test.ml
+++ src/analyses/wp_test.ml
@@ -14,9 +14,10 @@ struct
   let startstate v = D.empty()
   let exitstate v = D.empty()
 
-  let get_local = function
-    | Var v, _ -> Some v (* some local variable*)
-    | _, _ -> None
+  let vars_from_lval l = 
+    match l with
+    | Var v, _ -> Some v (* some  variable*)
+    | Mem _, _ -> None (*do not know what to do here yet*)
 
   let vars_from_expr (e: exp) : D.t=
     let rec aux acc e =
@@ -26,16 +27,20 @@ struct
         let acc1 = aux acc e1 in
         aux acc1 e2
       | UnOp (_, e1, _)       ->  aux acc e1
-      | CastE (_, e1)         ->   aux acc e1
       | SizeOfE e1            -> aux acc e1
       | AlignOfE e1           -> aux acc e1
-      |Question (e1, e2, e3, _) -> 
+      | Question (e1, e2, e3, _) -> 
         let acc1 = aux acc e1 in
         let acc2 = aux acc1 e2 in
         aux acc2 e3
+      | CastE (_, e1)         ->   aux acc e1
+      | AddrOf (l1)          ->   (match vars_from_lval l1 with
+          | None -> acc
+          | Some v -> D.add v acc)
       | _ -> acc 
     in
     aux (D.empty()) e
+
 
   let assign man (lval:lval) (rval:exp) =
     let () =
@@ -47,10 +52,10 @@ struct
       Logs.debug "  local is_bot: %b" (D.is_bot man.local);
     in
 
-    let v = get_local lval in
+    let v = vars_from_lval lval in
 
     match v with
-    | None -> Logs.debug "!!! possibly unsound !!!"; D.top ()
+    | None -> D.join man.local (vars_from_expr rval) (*if I do not know what the value is assigned to, then all RHS-Variables might be relevant*)
     | Some v -> 
       let l = (D.diff man.local (D.singleton v)) in
       if D.mem v man.local then D.join l (vars_from_expr rval)
@@ -61,15 +66,16 @@ struct
 
   let body man (f:fundec) =
     man.local
-
   let return man (exp:exp option) (f:fundec) =
     match exp with
     | None -> man.local
     | Some e -> D.join man.local (vars_from_expr e)
 
+  (* TODO *)
   let enter man (lval: lval option) (f:fundec) (args:exp list) =
     [man.local, man.local]
 
+  (* TODO *)
   let combine_env man (lval:lval option) fexp (f:fundec) (args:exp list) fc au (f_ask: Queries.ask) =
     au
 
